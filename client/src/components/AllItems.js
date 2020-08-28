fe5e8f6afeda3b38eb63de8e6a0df1b6684f7330
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-28 15:06:18 -0400

    AllItems Component now rendering Cars data from db

--- client/src/components/AllItems.js
+++ client/src/components/AllItems.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export default function AllItems() {
+    const [state, setState] = useState({
+        cars: []
+    })
+
+    useEffect(() => {
+        const getData = async() => {
+            try {
+                const response = await fetch('/items')
+                const data = await response.json()
+                setState({
+                    cars: data.map((car, i) => {
+                        return(
+                            <div key={i}>
+                                <img src={car.imageUrl}></img>
+                                <h3>{car.name}</h3>
+                                <h3>${car.price}.00</h3>
+                                <h3>{car.description}</h3>
+                                <hr></hr>
+                            </div>
+                        )
+                    })
+                })
+            } catch(er) {console.log(er)}
+        }
+        getData()
+    },[])
+
     return (
         <div>
-            This is all the cars
+            {state.cars}
         </div>
     )
 }
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,4 @@
 // this is where your components are exported
-// ex:
-// export {default as Projects} from './Projects.js'
-// export {default as About} from './About.js'
 export {default as Navbar} from './NavBar'
 export {default as Home} from './Home'
 export {default as AllItems} from './AllItems'

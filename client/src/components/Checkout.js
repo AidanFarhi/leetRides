f4eb4a0ec5cf8d519b25b1cd7a5c01323e10270d
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-31 19:24:48 -0400

    checkout now rendering all items and the total cost

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export default function Checkout() {
+    const [state, setState] = useState({
+        total: 0,
+        cartListItems: []
+    })
+
+    // const placeOrder = async() => {
+
+    // }
+
+    useEffect(()=> {
+       const getCart = async() => {
+           try {
+            const response = await fetch(`cart/${localStorage.getItem('id')}`)
+            const data = await response.json()
+            const listItems = data.map((car, i) => <li key={i}>{car.name} - {car.price}</li>)
+            const prices = data.map(car => car.price)
+            const reducer = (a, b) => a + b;
+            const totalCost = prices.reduce(reducer)
+            setState({
+                total: totalCost,
+                cartListItems: listItems
+            })
+           } catch(er) {console.log(er)}
+       } 
+       getCart()
+    }, [])
+
     return (
         <div>
-            This is all the Checkout Page
+            <h1>Checkout</h1>
+            <ul>
+                {state.cartListItems}
+            </ul>
+            <h2>Your total is: ${state.total}.00</h2>
+            {/* <button onClick={placeOrder}>Place Order</button> */}
         </div>
     )
 }

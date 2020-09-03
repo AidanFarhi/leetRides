2aea4117c06afc1bfff5c1133a9f38b500b4c3fb
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-09-02 21:36:56 -0400

    styled SinleUser

--- client/src/components/SingleUser.js
+++ client/src/components/SingleUser.js
@@ -1,10 +1,13 @@
 import React, {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import '../cmp-styles/SingleUser.css'
 
 export default function SingleUser(props) {
     const [state, setState] = useState({
         userData: {},
         // array of <div>s
-        carDivs: []
+        carLinks: [],
+        joinDate: ''
     })
 
     useEffect(()=> {
@@ -23,17 +26,16 @@ export default function SingleUser(props) {
                     })
                 })
                 const cars = await getCarsResponse.json()
-                const carDivs = cars.map((car, i) => {
+                const carLinks = cars.map((car, i) => {
                     return (
-                        <div key={i}>
-                            <img src={car.imageUrl}/>
-                            <p>{car.name}</p>
-                        </div>
+                        <Link key={i} to={{pathname:'/car', query:{id: car.id.toString()}}}><img className='car-order-image' src={car.imageUrl} alt='a nice car'></img></Link>
                     )
                 })
+                const dateJoined = user.createdAt.slice(0, 10)
                 setState({
                     userData: user,
-                    carDivs: carDivs
+                    carLinks: carLinks,
+                    joinDate: dateJoined
                 })
             } catch(er) {console.log(er)}
         }
@@ -41,15 +43,15 @@ export default function SingleUser(props) {
     },[])
 
     return(
-        <div>
+        <div id='single-user-main-div'>
             <div className='user-div'>
                 <img src={state.userData.imageUrl} alt='an user icon'></img>
-                <h3>{state.userData.name}</h3>
-                <h3>{state.userData.address}</h3>
-                <h3>{state.userData.email}</h3>
-                <hr></hr>
+                <h3>{state.userData.username}</h3>
+                <p>User Joined: {state.joinDate}</p>
                 <h3>Recently Ordered</h3>
-                {state.carDivs}
+            </div>
+            <div id='recent-order-car-links'>
+                {state.carLinks}
             </div>
         </div>
     )

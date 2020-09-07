5c54de51188cf9f97bf6b00f6256782374f2573e
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-09-07 15:40:56 -0400

    added default image on user creation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -38,6 +38,10 @@ router.post('/login', async(req, res, next) => {
     } catch(er) {next(er)}
 })
 
+const urlCheck = (url) => {
+    const reg = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/
+    return reg.test(url)
+}
 //route to create user
 router.post('/register', async(req, res, next) => {
     try {
@@ -58,11 +62,17 @@ router.post('/register', async(req, res, next) => {
             if (check[0].address === address) res.send({response: 'That address is already registered'})
             if (check[0].email === email) res.send({response: 'That email is already registered'})
         } else {
+            let finalUrl = ''
+            if (urlCheck(imageUrl)) {
+                finalUrl = imageUrl
+            } else {
+                finalUrl = 'https://freesvg.org/img/abstract-user-flat-4.png'
+            }
             const hash = await bcrypt.hash(password, 10)
             const newUser = await Users.create({
                 name: name,
                 username: username,
-                imageUrl: imageUrl,
+                imageUrl: finalUrl,
                 address: address,
                 email: email,
                 password: hash

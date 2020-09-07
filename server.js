7d9a10af1dcb4cd0693e3fe08b20f0e0df8e3cf8
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-09-07 18:34:52 -0400

    server changes

--- server.js
+++ server.js
@@ -34,6 +34,16 @@ app.use('/car/guest', require('./routes/guestRoutes'))
 app.use('/guest', require('./routes/guestRoutes'))
 app.use('/key', require('./routes/serveKeys'))
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 // Error catching endware
 app.use((err, req, res, next) => {
     console.error(err, typeof next)

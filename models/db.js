b5364fad4e7487b3fe22220f8cf06f5b8ea27655
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-09-07 18:18:25 -0400

    changed db url

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 // link to database goes here
 const Sequelize = require('sequelize')
-const uri = process.env.DATABASE_URI
+const uri = process.env.DATABASE_URL
 const db = new Sequelize(uri,{logging: false},)
 
 module.exports = db
